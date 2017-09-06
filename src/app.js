c6c615d2fe89323be9bdb1b7c6bf19cc8abb40c6
rachaelcodes <rorourke.rsp@gmail.com>
2017-09-06 10:51:18 +0100

    #14 started express server and added an interim favicon

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const exphbs = require('express-handlebars');
+const controllers = require('./controllers/index');
+const helpers = require('./controllers/helpers');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+    helpers: helpers
+  })
+);
+
+app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(controllers);
+
+module.exports = app;
---
+++ src/controllers/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const router = express.Router();
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+
+module.exports = router;
