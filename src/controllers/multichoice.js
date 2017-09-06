edd0e8fab31c5a39477c9ad655e994bfc0c7fefd
jen-harris <jennifer.louise.harris@gmail.com>
2017-09-06 11:49:55 +0100

    created controllers for home.js and multichoice q relates #11

---
+++ src/controllers/multichoice.js
@@ -0,0 +1,33 @@
+const dbConnection = require('./../model/db_connection');
+
+exports.get = (req, res, next) => {
+
+  // may need updating after testing!
+  const sqlQuery = `SELECT questions.content, questions.hint, extracts.content FROM questions INNER JOIN extracts ON questions.small_extract_id = extracts.id WHERE questions.id=${req.params.number}`;
+
+  const showData = () => {
+    return new Promise((resolve, reject) => {
+      dbConnection.query(sqlQuery,
+        (err, res) => {
+          if (err) {
+            reject(err, 'This is an error in the sqlQuery function');
+          } else {
+            resolve(res, 'response to the sqlQuery');
+          }
+        });
+    })
+  }
+  showData()
+    .then((queryResult, text) => {
+      res.render('multichoice', {
+        activePage: {
+          multichoice: true
+        },
+        data: queryResult.rows[0]
+      });
+    })
+    .catch((queryResult, text) => {
+      res.status(500);
+      return next();
+    });
+};
