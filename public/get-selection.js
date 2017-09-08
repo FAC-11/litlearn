ec0dbd7b09d529608ac2443c4c5e261c824e063a
Morkeltry <morkeltry@mailfence.com>
2017-09-08 14:53:16 +0100

    hopefully fixed second bug in #56; Relates #16

--- public/get-selection.js
+++ public/get-selection.js
@@ -2,7 +2,7 @@ function showSelection () {
   var selection = JSON.parse (sessionStorage.getItem("selection"));
   var output = document.createElement ('div');
   output.innerHTML = '<h1>You selected: '+selection.selection+'</h1>'+
-  '<h2>You\'ve done '+startTimer(false)+' mins this session</h2>';
+  '<h2>You\'ve done '+startTimer(true)+' mins this session</h2>';
   document.body.appendChild (output);
 }
 
