0d9b6dd3b6c82a6d93c9545ff80c95816bcbb4db
rachaelcodes <rorourke.rsp@gmail.com>
2017-09-08 16:36:05 +0100

    #10 updated so it now prefills with the answer from multichoice

--- src/controllers/explain.js
+++ src/controllers/explain.js
@@ -4,7 +4,8 @@ exports.get = (req, res, next) => {
 
   // may need updating after testing!
   const sqlQuery = `SELECT questions.questioncontent, questions.hint, extracts.extractcontent FROM questions INNER JOIN extracts ON questions.small_extract_id = extracts.id WHERE questions.id=${req.params.number}`;
-
+  const optionList = ['answer 1', 'answer 2', 'answer 3', 'answer 4'];
+  const selected = optionList[req.query.answer];
   const showData = () => {
     return new Promise((resolve, reject) => {
       dbConnection.query(sqlQuery,
@@ -24,7 +25,7 @@ exports.get = (req, res, next) => {
         activePage: {
           explain: true
         },
-        data: Object.assign(queryResult.rows[0], {options: ['answer 1', 'answer 2', 'answer 3', 'answer 4']})
+        data: Object.assign(queryResult.rows[0], {options: selected})
       });
     })
     .catch((queryResult, text) => {
