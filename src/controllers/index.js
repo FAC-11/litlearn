09a41953c7bd62cf87bc91c56bcb98ee9b96f343
Rachael <rachaelcodes@users.noreply.github.com>
2017-09-08 12:02:27 +0100

    Update index.js
    allowed for different extract endpoints according to question

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -15,5 +15,5 @@ router.get('/', home.get);
 // included /:number to allow for random questions
 router.get('/multichoice/:number', multichoice.get);
 router.get('/congratulations', congratulations.get);
-router.get('/extract', extract.get);
+router.get('/extract/:number', extract.get);
 module.exports = router;
