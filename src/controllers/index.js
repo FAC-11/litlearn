056d11feffbadd8895fc49b840c338afccc9620b
jen-harris <jennifer.louise.harris@gmail.com>
2017-09-08 12:42:10 +0100

    created design for summary page relates #3 left space for nav bar so may need editing once this is in place currently has holding values which will need updating

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,10 +8,12 @@ router.use(bodyParser.urlencoded({extended: true}));
 const home = require('./home');
 const multichoice = require('./multichoice');
 const congratulations = require('./congratulations');
+const summary = require('./summary');
 
 
 router.get('/', home.get);
 // included /:number to allow for random questions
 router.get('/multichoice/:number', multichoice.get);
+router.get('/summary', summary.get);
 router.get('/congratulations', congratulations.get);
 module.exports = router;
