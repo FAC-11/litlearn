1e84564950ec52dfdc03f53f71ae83bab0b470b8
rachaelcodes <rorourke.rsp@gmail.com>
2017-09-11 15:10:54 +0100

    adding new SQL query before I try a trickier one #22

---
+++ src/controllers/choosequestion.js
@@ -0,0 +1,33 @@
+const dbConnection = require('./../model/db_connection');
+
+exports.get = (req, res, next) => {
+
+  const sqlQuery = `SELECT questions.id, questions.summary, string_agg(tags.tag, ', ') AS filter FROM questions INNER JOIN many2many ON questions.id=many2many.question_id INNER JOIN tags on many2many.tag_id = tags.id GROUP BY questions.summary, questions.id;`;
+
+  const showData = () => {
+    return new Promise((resolve, reject) => {
+      dbConnection.query(sqlQuery,
+        (err, res) => {
+          if (err) {
+            reject(err, 'This is an error in the sqlQuery function');
+          } else {
+            resolve(res, 'response to the sqlQuery');
+          }
+        });
+    })
+  }
+  showData()
+    .then((queryResult, text) => {
+      res.render('choosequestion', {
+//activePage for potential future functionality
+        activePage: {
+          choosequestion: true
+        },
+        data: queryResult.rows
+      });
+    })
+    .catch((queryResult, text) => {
+      res.status(500);
+      return next();
+    });
+};
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,6 +7,7 @@ router.use(bodyParser.urlencoded({extended: true}));
 
 const home = require('./home');
 const multichoice = require('./multichoice');
+const choosequestion = require('./choosequestion');
 const congratulations = require('./congratulations');
 const extract = require('./extract');
 const intro = require('./intro');
@@ -14,6 +15,7 @@ const intro = require('./intro');
 
 router.get('/', home.get);
 // included /:number to allow for random questions
+router.get('/choosequestion', choosequestion.get);
 router.get('/multichoice/:number', multichoice.get);
 router.get('/intro/:number', intro.get);
 router.get('/congratulations', congratulations.get);
