85631746d992ed484d51938a6f53627d7b438c76
rachaelcodes <rorourke.rsp@gmail.com>
2017-09-11 17:58:57 +0100

    #22 basic page rendering, plus data now passes to the filter.js as a data attribute Jen has suggested some more styling for the form at: https://codepen.io/jen-harris/pen/KXPmeR

--- src/controllers/choosequestion.js
+++ src/controllers/choosequestion.js
@@ -2,20 +2,22 @@ const dbConnection = require('./../model/db_connection');
 
 exports.get = (req, res, next) => {
 
-  const sqlQuery = `SELECT questions.id, questions.summary, string_agg(tags.tag, ', ') AS filter FROM questions INNER JOIN many2many ON questions.id=many2many.question_id INNER JOIN tags on many2many.tag_id = tags.id GROUP BY questions.summary, questions.id;`;
+  const sqlQuery = `SELECT questions.summary, questions.id, string_agg(tags.tag, ', ')
+ AS filter FROM questions INNER JOIN many2many ON questions.id=many2many.question_id INNER JOIN tags on many2many.tag_id = tags.id GROUP BY questions.summary, questions.id;`;
 
   const showData = () => {
-    return new Promise((resolve, reject) => {
-      dbConnection.query(sqlQuery,
-        (err, res) => {
-          if (err) {
-            reject(err, 'This is an error in the sqlQuery function');
-          } else {
-            resolve(res, 'response to the sqlQuery');
-          }
-        });
-    })
-  }
+      return new Promise((resolve, reject) => {
+        dbConnection.query(sqlQuery,
+          (err, res) => {
+            if (err) {
+              reject(err, 'This is an error in the sqlQuery function');
+            } else {
+              resolve(res, 'response to the sqlQuery');
+            }
+          });
+      })
+    }
+
   showData()
     .then((queryResult, text) => {
       res.render('choosequestion', {
