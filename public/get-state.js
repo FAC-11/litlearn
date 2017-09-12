d530e7406a326cd064bdde5e7546846d7064e1b7
Morkeltry <morkeltry@mailfence.com>
2017-09-12 12:49:42 +0100

    get-state.js adds data found in sessionStorage under item X to an attribute (currently data-Y) of an HTML tag Y if found; Relates #54, #43

--- public/get-state.js
+++ public/get-state.js
@@ -1,38 +1,59 @@
 ///front end - called as <script src=...>
 
-function getState () {
-  console.log (document.keys.length);
-  const retrieved = {};
-    questionObjectString : window.sessionStorage.getItem ('question-data',questionObjectString),
-    answerObjectString : window.sessionStorage.getItem ('answers',answerObjectString),
-    oneTaskTimer : window.sessionStorage.getItem ('oneTaskTimer',oneTaskTimer),
-    totalTimer : window.sessionStorage.getItem ('totalTimer',totalTimer)
+
+// {key=sessionStorage name : {html: HTML id}}
+const ids=
+  {'question-content' : {'html':'question-content' },
+  'answers' : {'html': 'answers'},
+  'oneTaskTimer' : {'html': 'timer'},
+  // 'totalTimer' : {'html': 'totalTimer' }
   };
-  return ('<h1>'+JSON.stringify(retrieved)+'<h1>');
+
+  // elements within 'question-data':
+  //
+  // intro : 'question-summary'
+  // multichoice: question-content
+  // multichoice: options
+  // multichoice: hint
+  //
+  //
+
+// {key=sessionStorage name : {html: HTML id, element: actual HTML element withih document}}
+function locateCurrentElements (idsObj) {
+  const elements = {};
+console.log (Object.keys(idsObj));
+    Object.keys(idsObj).forEach ((el) => {
+console.log (el,' :-');
+      var thisElement = document.getElementById(idsObj[el].html);
+      if (thisElement) {
+        console.log ('adding ',el,' to ',idsObj[el].html);
+        elements[el]={};
+        elements[el].html = idsObj[el].html
+        elements[el].element = thisElement;
+      }
+    });
+  return elements;
 }
 
-function loadDataToElements (toLoad) {
-  const elements={};
-  ['question-data',
-    'answers',
-    'oneTaskTimer',
-    'totalTimer']
-      .forEach ((el) => {elements[el] = document.GetElementById(el)}); ///not quite right!
+// {key=sessionStorage name : {html: HTML id, element: actual HTML element withih document}}
+function retrieveState () {
+  // console.log (document.keys.length);
+  const elementsObj = locateCurrentElements(ids);
+  console.log ('_____________________________________');
+  console.log ('elementsObj=locateCurrentElements(ids): ',elementsObj);
+  console.log ('locateCurrentElements(ids).keys.length: ',Object.keys(elementsObj).length);
 
+  Object.keys(elementsObj).forEach ((el) => {
+    console.log ('Obj.',el,' = ',elementsObj[el]);
+    console.log ('Obj.',el,'.html = ',elementsObj[el].html);
+    console.log ('Obj.',el,'.element = ',elementsObj[el].element);
+    ///Use of .innerHTML as calculated security risk. HTML likely to be used.
+    ///Gives malicious user only  the opportunity to run arbitrary code as our domain on
+    ///own machine - no biggie...
+    elementsObj[el].element.setAttribute
+          ('data-'+elementsObj[el].html, window.sessionStorage.getItem (el));
+  });
 }
-
-// elements within 'question-data':
-//
-// intro : 'question-summary'
-// multichoice: question-content
-// multichoice: options
-// multichoice: hint
-//
-//
-//
-//
-
-
 
 document.getElementById ('question-object-storage-container')
-  .setAttribute('data-question-object', getState ());
+  .setAttribute('data-question-object', retrieveState ());
