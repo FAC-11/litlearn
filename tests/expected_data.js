e5f6f50fe099bcea00c2bcfdb276aeb76185ae04
rachaelcodes <rorourke.rsp@gmail.com>
2017-09-13 12:47:50 +0100

    added DB tests

---
+++ tests/expected_data.js
@@ -0,0 +1,5 @@
+module.exports = {
+  users: {username: [{username:'owen'}], hashedPassword:[{hashedPassword: '$2a$10$jjczKwDy/RYmdFTI.KUY0e.QToOk.fHQcBpOLpF8t8Ulg6dvy5TaW'}]},
+  questions: {summary: [{summary: 'an Edgar Allen Poe story opening'}, {summary: 'a 19th Century theatre review'},{summary: 'a scene between Benvolio and Tybalt'}]
+  }
+}
--- tests/test.js
+++ tests/test.js
@@ -2,13 +2,12 @@ const test = require('tape');
 const supertest = require('supertest');
 const app = require('./../src/app');
 const endpoints = require('./endpoints');
+const expected_data = require('./expected_data');
+const dbConnection = require('../src/model/db_connection');
 
-test('1 equals 1', t => {
-  t.equals(1, 1, 'one should equal one');
-  t.end();
-});
+//endpoint tests
 
-test('check if supertest works', t => {
+test('check home route', t => {
   supertest(app)
     .get('/')
     .expect(200)
@@ -53,4 +52,76 @@ test('check intro endpoint not working for non-existent questions', t => {
         t.same(res.statusCode, 404, 'intro/32904 status code is 404');
       })
   t.end();
+})
+
+
+//database tests
+test('Select all summaries from questions', t => {
+  const expected = expected_data.questions.summary;
+
+  dbConnection.query('SELECT summary FROM questions;', (err, res) => {
+    if (err) {
+      t.error(err, 'This is an error');
+    } else {
+      const actual = res.rows;
+      t.deepEquals(actual, expected, 'db query should get questions');
+      t.end();
+    }
+  })
+})
+
+test('Select all usernames from users', t => {
+  const expected = expected_data.users.username;
+
+  dbConnection.query('SELECT username FROM users;', (err, res) => {
+    if (err) {
+      t.error(err, 'This is an error');
+    } else {
+      const actual = res.rows;
+      t.deepEquals(actual, expected, 'db query should get users');
+      t.end();
+    }
+  })
+})
+
+test('Check number of extracts', t => {
+  const expected = 3;
+
+  dbConnection.query('SELECT * FROM extracts;', (err, res) => {
+    if (err) {
+      t.error(err, 'This is an error');
+    } else {
+      const actual = res.rows.length;
+      t.deepEquals(actual, expected, 'db query should get extracts');
+      t.end();
+    }
+  })
+})
+
+test('Check number of tags', t => {
+  const expected = 17;
+
+  dbConnection.query('SELECT * FROM tags;', (err, res) => {
+    if (err) {
+      t.error(err, 'This is an error');
+    } else {
+      const actual = res.rows.length;
+      t.deepEquals(actual, expected, 'db query should get tags');
+      t.end();
+    }
+  })
+})
+
+test('Check number of many2many', t => {
+  const expected = 19;
+
+  dbConnection.query('SELECT * FROM many2many;', (err, res) => {
+    if (err) {
+      t.error(err, 'This is an error');
+    } else {
+      const actual = res.rows.length;
+      t.deepEquals(actual, expected, 'db query should get many2many');
+      t.end();
+    }
+  })
 })
