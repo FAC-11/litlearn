0f977ac3e4a9aa16a99c4902e459dbfa68287740
Morkeltry <morkeltry@mailfence.com>
2017-09-13 18:58:59 +0100

    timer functions work and don't break. #54 #74

--- public/get-state.js
+++ public/get-state.js
@@ -52,12 +52,12 @@ var ids=
 // {key=sessionStorage name : {html: HTML id, element: actual HTML element withih document}}
 function locateCurrentElements (idsObj) {
   const elements = {};
-console.log (Object.keys(idsObj));
+// console.log (Object.keys(idsObj));
     Object.keys(idsObj).forEach ((el) => {
-console.log (el,' :-');
+// console.log (el,' :-');
       var thisElement = document.getElementById(idsObj[el].html);
       if (thisElement) {
-        console.log ('adding ',el,' to ',idsObj[el].html);
+        // console.log ('adding ',el,' to ',idsObj[el].html);
         elements[el]=Object.assign(idsObj[el],{element:thisElement });
         // elements[el].html = idsObj[el].html
         // elements[el].element = thisElement;
@@ -72,26 +72,26 @@ function retrieveState () {
   var destAttr = '';
   // console.log (document.keys.length);
   var elementsObj = locateCurrentElements(ids);
-  console.log ('_____________________________________');
-  console.log ('elementsObj=locateCurrentElements(ids): ',elementsObj);
-  console.log ('locateCurrentElements(ids).keys.length: ',Object.keys(elementsObj).length);
+  // console.log ('_____________________________________');
+  // console.log ('elementsObj=locateCurrentElements(ids): ',elementsObj);
+  // console.log ('locateCurrentElements(ids).keys.length: ',Object.keys(elementsObj).length);
 
   Object.keys(elementsObj).forEach ((el) => {
-    console.log ('Obj.',el,' = ',elementsObj[el]);
-    console.log ('Obj.',el,'.html = ',elementsObj[el].html);
-    console.log ('Obj.',el,'.element = ',elementsObj[el].element);
+    // console.log ('Obj.',el,' = ',elementsObj[el]);
+    // console.log ('Obj.',el,'.html = ',elementsObj[el].html);
+    // console.log ('Obj.',el,'.element = ',elementsObj[el].element);
     ///Intended with use of attribute .innerHTML as calculated security risk because HTML
     ///likely to be used in our data.
     ///Gives malicious user only the opportunity to run arbitrary code as our domain on
     ///own machine - no biggie...
-console.log ('dest if any = ',destAttr = elementsObj[el]);
+// console.log ('dest if any = ',destAttr = elementsObj[el]);
     destAttr = elementsObj[el].dest || 'data-'+elementsObj[el].html;  ///currently endsup in eg
                                                             ///<h2>.dataset.camelCaseAttr = 'contents'
     elementsObj[el].element.setAttribute
           (destAttr, window.sessionStorage.getItem (el));
     elementsObj[el].element[destAttr]= window.sessionStorage.getItem (el);
     // Try both ways to be sure. IE only likes get/setAttr, but doesn't work for innerHTML/ textContent which are properties not attributes. Need to further investigate and test, esp IE.
-  console.log ('setting into',destAttr,'of',elementsObj[el].html,'found at',elementsObj[el].element);
+  // console.log ('setting into',destAttr,'of',elementsObj[el].html,'found at',elementsObj[el].element);
 
   });
 }
